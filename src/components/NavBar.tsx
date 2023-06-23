fe6ab1b86a9ce3eba874d07a59b09f2b232fd1d6
Payam Yektamaram <payamyek@gmail.com>
2023-06-23 18:54:30 -0400

    refactor: code cleanup

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -48,11 +48,11 @@ const ThemeSelectorDropdownItem = (props: { value: string }) => {
       >
         <div className=' grid grid-cols-5 grid-rows-3'>
           <div className='col-span-5 row-span-3 row-start-1 flex items-center gap-2 p-3'>
-            <div className='flex-grow text-sm ml-4'>{props.value}</div>{' '}
+            <div className='flex-grow text-sm ml-4'>{props.value}</div>
             <div className='flex h-full flex-shrink-0 flex-wrap gap-1'>
-              <div className='bg-primary w-2 rounded'></div>{' '}
-              <div className='bg-secondary w-2 rounded'></div>{' '}
-              <div className='bg-accent w-2 rounded'></div>{' '}
+              <div className='bg-primary w-2 rounded'></div>
+              <div className='bg-secondary w-2 rounded'></div>
+              <div className='bg-accent w-2 rounded'></div>
               <div className='bg-neutral w-2 rounded'></div>
             </div>
           </div>
