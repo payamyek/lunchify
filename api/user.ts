db1bb908ef717819bce5d9dafb4b2498b95b8fa1
Payam Yektamaram <payamyek@gmail.com>
2023-06-25 14:36:02 -0400

    feat: serverless function for webhooks

---
+++ api/user.ts
@@ -0,0 +1,35 @@
+//import type { WebhookEvent } from '@clerk/clerk-sdk-node';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+import { Webhook } from 'svix';
+
+export default async function handler(
+  request: VercelRequest,
+  response: VercelResponse
+) {
+  // only allow POST calls
+  if (request.method !== 'POST') {
+    return response
+      .status(405)
+      .json({ message: `${request.method} method not allowed` });
+  } else if (process.env.USER_WEBHOOK_SECRET === undefined) {
+    return response
+      .status(500)
+      .json({ message: 'Server function has misconfigured webhook secret' });
+  }
+
+  const wh = new Webhook(process.env.USER_WEBHOOK_SECRET);
+
+  let msg;
+
+  try {
+    msg = wh.verify(request.body, request.headers);
+  } catch (err) {
+    return response
+      .status(400)
+      .json({ message: 'Webhook verification failed' });
+  }
+
+  response.status(200).json({
+    message: msg,
+  });
+}
